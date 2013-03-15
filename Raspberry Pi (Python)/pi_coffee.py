8c609f7c940dbcf589534f3d47522a1c27db11f7
Jon Arnett <jonarnett90@gmail.com>
2013-03-15 14:55:07 -0400

    Began work on RPi GPIO

---
+++ Raspberry Pi (Python)/pi_coffee.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+import time
+import RPi.GPIO as GPIO
+import httplib
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(4, GPIO.OUT)
+GPIO.setup(18, GPIO.IN)
+
+oldSec = time.strftime("%S")
+
+while True:
+    
+    print time.strftime("%a %b %d %H:%M:%S")
+    
+    conn = httplib.HTTPConnection("ruby-coffee-maker.herokuapp.com")
+    conn.request("GET", "/coffee")
+    resp = conn.getresponse()
+    data = resp.read()
+    
+    if data == "1":
+        print "Should make coffee!"
+    else:
+        print "Should not make coffee!"
+        
+    conn.close()
+    
+    time.sleep(1.0)
