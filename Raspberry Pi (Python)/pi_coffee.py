45b4b3d0245e54b2771ea5e0d8ddda669b21e35f
Jon Arnett <jonarnett90@gmail.com>
2013-03-17 00:49:59 -0400

    Added full functionality

--- Raspberry Pi (Python)/pi_coffee.py
+++ Raspberry Pi (Python)/pi_coffee.py
@@ -5,27 +5,55 @@ import time
 import RPi.GPIO as GPIO
 import httplib
 
+oldMin = ""
+oldSec = ""
+
+coffeeTimes = {"Sun 10:00", 
+               "Mon 9:20", 
+               "Tue 8:10", 
+               "Wed 8:10",
+               "Thu 8:10",
+               "Fri 8:10"
+               "Sat 10:00"}
+
+brewPin = 10
+stopPin = 9
+
+souldBrew = False
+brewing = False
+
 GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(10, GPIO.OUT)
-GPIO.setup(9, GPIO.IN)
-
-oldSec = time.strftime("%S")
+GPIO.setup(brewPin, GPIO.OUT)
+GPIO.setup(stopPin, GPIO.IN)
 
 while True:
-    
-    print time.strftime("%a %b %d %H:%M:%S")
-    
-    conn = httplib.HTTPConnection("ruby-coffee-maker.herokuapp.com")
-    conn.request("GET", "/coffee")
-    resp = conn.getresponse()
-    data = resp.read()
-    
-    if data == "1":
-        print "Should make coffee!"
-    else:
-        print "Should not make coffee!"
+    if time.strftime("%M") != oldMin:
+        wDay = time.strftime("%a")
+        month = time.strftime("%b")
+        mDay = time.strftime("%d")
+        timeString = time.strftime("%H:%M")
         
-    conn.close()
+        if (wDay + " " + timeString) in coffeeTimes:
+            shouldBrew = True
+        
+        print wDay, month, mDay, timeString
     
-    time.sleep(1.0)
+    if time.strftime("%S") != oldSec:
+        conn = httplib.HTTPConnection("ruby-coffee-maker.herokuapp.com")
+        conn.request("GET", "/coffee")
+        resp = conn.getresponse()
+        data = resp.read()
+        
+        if data == "1":
+            print "Should make coffee!"
+            shouldBrew = True
+        else:
+            print "Should not make coffee!"
+        
+        conn.close()
+    
+    if shouldBrew:
+        GPIO.output(brewPin, GPIO.HIGH)
+    
+    if GPIO.input(stopPin):
+        GPIO.output(brewPin, GPIO.LOW)
