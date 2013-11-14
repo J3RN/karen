3b6b9d85540e7fbc307fa7d5fc36518b4265ef3b
Jonathan Arnett <jonarnett90@gmail.com>
2013-11-14 11:53:11 -0500

    Added the views for Karens and added creation to the controller

--- webapp/app/controllers/karens_controller.rb
+++ webapp/app/controllers/karens_controller.rb
@@ -1,4 +1,5 @@
 class KarensController < ApplicationController
+  before_action :load_karen, only: [:show, :edit, :update, :destroy]
 
   def index
     @karens = Karen.all
@@ -8,9 +9,17 @@ class KarensController < ApplicationController
   end
 
   def new
+    @karen = Karen.new
   end
 
   def create
+    @karen = Karen.new(karen_params)
+    
+    if @karen.save
+      redirect_to karen_path(@karen), notice: "Karen successfully added"
+    else
+      redirect_to new_karen_path(@karen), notice: "Karen could not be saved"
+    end
   end
 
   def edit
@@ -22,4 +31,12 @@ class KarensController < ApplicationController
   def destroy
   end
 
+  private
+    def load_karen
+      @karen = Karen.find(params[:id])
+    end
+
+    def karen_params
+      params.require(:karen).permit(:name)
+    end
 end
